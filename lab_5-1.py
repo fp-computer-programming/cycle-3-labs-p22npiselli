cb80f340734115472d769a9760fbb29fbb4c453e
p22npiselli <p22npiselli@fairfieldprep.org>
2021-10-04 09:56:53 -0400

    lab 5-1

---
+++ lab_5-1.py
@@ -0,0 +1,17 @@
+# Author: Nolan (AMDG) 10/4/2021
+
+# a = 1
+# int(a)
+# NameError
+
+# int('a')
+# ValueError
+
+# 'a'+2
+# TypeError
+
+# import date
+# ModuleNotFoundError
+
+# print("I'm a happy camper!)
+# SyntaxError
